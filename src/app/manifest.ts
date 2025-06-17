d85051dfd7f1a0277264a18c4f178eb61c05e152
asHOH <asHOH@users.noreply.github.com>
2025-06-18 00:55:02 +0800

    docs(SEO): update description to be more detailed

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -6,7 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
   return {
     name: '猫和老鼠手游wiki',
     short_name: '猫鼠wiki',
-    description: '老鼠手游数据查询网站',
+    description: '猫和老鼠手游wiki - 提供详细的角色属性、技能、加点、知识卡查询推荐等数据和攻略',
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
