1e8f2f2b2f93d89e23c925644125a8f72ff56dd3
asHOH <asHOH@users.noreply.github.com>
2025-06-28 03:41:00 +0800

    fix(contributors): update contributor  structure and data

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -1,59 +1,92 @@
+// Defining the structure for a contributor
 export interface Contributor {
   id: string; // Unique identifier
   name: string; // Display name
   url?: string; // Link to their personal homepage
   roles: Role[]; // Array of roles they have fulfilled
-  isMinor: boolean; // To quantify the contribution
 }
 
 // Defining the structure for roles
 export interface Role {
   type: RoleType;
   characters?: string[]; // Optional: characters they have worked on
+  isMinor?: boolean; // Optional: to quantify the contribution, defaults to major (false)
 }
 
 // Enum for the different types of roles
 export enum RoleType {
-  ArtProvider = '提供美术素材',
+  ArtProvider = '美术素材提供',
   DataTester = '数据测试',
-  Copywriter = '文案编辑',
-  CopyEditor = '文案审核',
-  Developer = '项目开发',
-  VideoCreator = '教学视频',
+  ContentWriter = '文案撰写',
+  ContentProofreader = '文案校对',
+  Developer = '代码开发',
+  VideoCreator = '教学视频制作',
 }
 
 // The list of all contributors
 export const contributors: Contributor[] = [
   {
     id: 'asHOH',
-    name: 'asHOH',
+    name: '小曙光',
     url: 'https://github.com/asHOH',
     roles: [
-      { type: RoleType.Copywriter },
-      { type: RoleType.CopyEditor },
+      {
+        type: RoleType.ContentWriter,
+        characters: ['杰瑞', '泰菲', '尼宝', '汤姆', '布奇', '托普斯'],
+      },
+      { type: RoleType.ContentProofreader },
       { type: RoleType.Developer },
     ],
-    isMinor: false,
+  },
+  {
+    id: '3swordman',
+    name: '海阔天空',
+    url: 'https://github.com/3swordman',
+    roles: [
+      { type: RoleType.Developer },
+      {
+        type: RoleType.ContentWriter,
+        characters: ['侦探杰瑞', '航海士杰瑞', '苏蕊'],
+      },
+    ],
+  },
+  {
+    id: 'chaserTom',
+    name: '追风汤姆',
+    url: 'https://space.bilibili.com/3493135485241940',
+    roles: [
+      {
+        type: RoleType.ContentWriter,
+        characters: ['蒙金奇'],
+      },
+    ],
+  },
+  {
+    id: 'musicianJerry',
+    name: '音乐家杰瑞',
+    roles: [{ type: RoleType.ContentWriter, characters: ['罗宾汉泰菲'] }],
   },
   {
     id: 'dreamback',
     name: '梦回_淦德蒸蚌',
     url: 'https://space.bilibili.com/1193776217',
     roles: [{ type: RoleType.DataTester }],
-    isMinor: false,
   },
   {
     id: 'momo',
     name: '是莫莫喵',
     url: 'https://space.bilibili.com/443541296',
-    roles: [{ type: RoleType.DataTester }],
-    isMinor: false,
+    roles: [
+      {
+        type: RoleType.DataTester,
+        characters: ['杰瑞'],
+      },
+    ],
   },
   {
     id: 'fanshuwa',
     name: '凡叔哇',
     url: 'https://space.bilibili.com/273122087',
     roles: [{ type: RoleType.ArtProvider }],
-    isMinor: false,
   },
 ];
