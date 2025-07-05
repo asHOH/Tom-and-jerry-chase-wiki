fb785d1e3663960d0ae9cf7722fe866e8d34add4
asHOH <asHOH@users.noreply.github.com>
2025-07-06 07:17:11 +0800

    chore(jest): fix line ending of jest.setup.cjs

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -1,6 +1,5 @@
 require('@testing-library/jest-dom');
 
-// Polyfill for structuredClone
 if (!global.structuredClone) {
   global.structuredClone = (obj) => {
     return JSON.parse(JSON.stringify(obj));
