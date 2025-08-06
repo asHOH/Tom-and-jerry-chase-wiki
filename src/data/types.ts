3fc6e9730cfa92e4516e48545e02af24c6beed19
ConductorJerry <1273590260@qq.com>
2025-08-07 00:02:46 +0800

    feat(items): add more new items

--- src/data/types.ts
+++ src/data/types.ts
@@ -208,7 +208,7 @@ export type SpecialSkill = SpecialSkillDefinition & {
   imageUrl: string;
 };
 
-export type Itemtypelist = '投掷类' | '手持类' | '物件类' | '食物类' | '流程类'; //list of items' types
+export type Itemtypelist = '投掷类' | '手持类' | '物件类' | '食物类' | '流程类' | '其它'; //list of items' types
 export type Itemsourcelist = '常规道具' | '地图道具' | '技能道具'; //list of items' source
 
 export type ItemDefinition = {
