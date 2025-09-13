88c5a1dc7be8a23765355ee40a595e54445ac857
ConductorJerry <1273590260@qq.com>
2025-09-13 16:34:30 +0800

    feat(specialSkills): complete advice page

--- src/components/displays/special-skills/special-skill-advice/SpecialSkillAdivceData.ts
+++ src/components/displays/special-skills/special-skill-advice/SpecialSkillAdivceData.ts
@@ -2,8 +2,6 @@ import { FactionId } from '@/data/types';
 
 export type SpecialSkillAdviceDefinition = {
   description?: string;
-  detailedDescription?: string;
-  isMinor: boolean;
 };
 export type SpecialSkillAdvice = SpecialSkillAdviceDefinition & {
   name: string;
@@ -20,87 +18,59 @@ export const getMouseSpecialSkillImageUrl = (name: string): string => {
 };
 
 const catSpecialSkillDefinitions: Record<string, SpecialSkillAdviceDefinition> = {
-  蓄力重击: {
-    description: '适合大部分猫咪。',
-    detailedDescription: '适合绝大多数猫咪，尤其适合缺乏霸体能力的猫咪。用于打架或强绑火箭。',
-    isMinor: false,
-  },
   绝地反击: {
-    description: '适合大部分有控制但缺乏伤害的猫咪。',
-    detailedDescription:
-      '适合有控制但缺乏伤害的猫咪，可搭配“击晕”提高命中率。用于补充伤害或强行击倒高Hp老鼠。',
-    isMinor: false,
+    description: '适合**绝大部分**猫咪，尤其适合缺乏霸体能力的猫咪。用于打架、解控或强绑火箭。',
+  },
+  蓄力重击: {
+    description:
+      '适合**有控制但缺乏伤害**的猫咪，可搭配{击晕}提高命中率。用于补充伤害或强行击倒高Hp老鼠。',
   },
   急速翻滚: {
-    description: '适合大部分需要额外位移能力的猫咪。',
-    detailedDescription: '适合需要额外位移能力的猫咪。用于赶路或追击。',
-    isMinor: false,
+    description: '适合**需要额外位移能力**的猫咪。用于赶路或追击，提高机动性。',
   },
   全垒打: {
-    description: '适合受攻击增伤和移速加成收益较高的猫咪。',
-    detailedDescription:
-      '适合受攻击增伤和移速加成收益较高的猫咪，这类猫咪通常有受攻击增伤加成的技能。通常用于在自身投出投掷物后将其击飞，以此获得一段时间的攻击增伤和移速，提高爆发能力。推荐有一定熟练度后再使用该特技。',
-    isMinor: true,
+    description:
+      '适合**受攻击增伤和移速加成的收益较高**的猫咪。通常用于在自身投出道具后将其击飞，以此获得一段时间的攻击增伤和移速，提高爆发能力。推荐有一定熟练度后再使用该特技。',
   },
   '我生气了！': {
-    description: '适合少部分爪刀命中率高，或是有其他爪刀CD缩减能力的猫咪。',
-    detailedDescription:
-      '适合少部分爪刀命中率高，或是有其他爪刀CD缩减能力的猫咪。用于短时间内提高伤害和爆发能力，也可以搭配其他爪刀CD缩减效果将爪刀CD缩减至1秒以内，然后配合“击晕”实现无限控制。',
-    isMinor: true,
+    description:
+      '适合**少部分爪刀命中率高，或是有其他爪刀CD缩减能力**的猫咪。用于短时间内提高伤害和爆发能力，或是搭配其他爪刀CD缩减效果将爪刀CD缩减至1秒以内，以配合{击晕}实现连控。',
   },
   应急治疗: {
-    description: '适合极少部分需要额外恢复能力的猫咪。',
-    detailedDescription:
-      '适合极少部分需要额外恢复能力的猫咪。用于提高续航能力，减少吃食物浪费的时间。',
-    isMinor: true,
+    description: '适合**极少部分需要额外恢复能力**的猫咪。用于提高续航能力，减少吃食物浪费的时间。',
   },
   勇气爪击: {
-    description: '几乎没有猫咪携带此特技。',
-    isMinor: true,
+    description: '暂无猫咪适配该特技。',
   },
   长爪一击: {
-    description: '几乎没有猫咪携带此特技。',
-    isMinor: true,
+    description: '暂无猫咪适配该特技。',
   },
 };
 const mouseSpecialSkillDefinitions: Record<string, SpecialSkillAdviceDefinition> = {
   魔术漂浮: {
-    description: '适合大部分老鼠。在部分地图有奇效。',
-    detailedDescription:
-      '适合大部分老鼠。通常用于赶路或逃跑，也可以搭配“舍己”在救援后立刻释放技能避免踩到老鼠夹。此外，夏日游轮、游乐场、森林牧场、御门酒店等地图中的部分地形可通过魔术漂浮进行快速移动，逼迫猫咪绕远路，达到拉扯逃跑的作用。',
-    isMinor: false,
+    description:
+      '适合**大部分**老鼠。用于赶路或逃跑，也可以在进行火箭救援后立刻释放特技以避免踩到{老鼠夹}。此外，[部分高低差地形](部分房间下方的地面允许角色自下而上通行，可利用漂浮直接到上方。包括夏日游轮的甲板、御门酒店的休息厅、部分雪夜古堡的活板门等；此外，游乐场的摩天轮和高台滑道、森林牧场的森林等房间也可以利用漂浮快速移动)可通过魔术漂浮进行快速移动，逼迫猫咪绕远路，达到拉扯逃跑的作用。',
   },
   绝处逢生: {
-    description: '适合大部分老鼠。',
-    detailedDescription:
-      '适合大部分老鼠。通常用于在倒地后进行自愈和治疗，此外也可以利用清除控制效果的特性来清理老鼠夹（特技效果触发时会解除被夹住状态）。',
-    isMinor: false,
-  },
-  应急治疗: {
-    description: '适合需要额外恢复能力的老鼠。',
-    detailedDescription: '适合需要额外恢复能力的老鼠。用于解除受伤和进行治疗。',
-    isMinor: false,
+    description:
+      '适合**大部分**老鼠。用于在倒地后进行自愈和治疗，也可以利用[清除控制效果的特性](特技效果触发时会解除被夹住状态)来清理{老鼠夹}。',
   },
   干扰投掷: {
-    description: '适合需要牵制猫咪的老鼠。',
-    detailedDescription: '适合需要牵制猫咪的老鼠。用于补充控制链或打断敌方的动作。',
-    isMinor: false,
+    description: '适合**需要干扰或牵制猫咪**的老鼠。用于补充控制或打断动作。',
+  },
+  应急治疗: {
+    description:
+      '适合**需要额外恢复能力**的老鼠。用于进行治疗或解除[部分不良状态](包括受伤、香水反向、烟雾失明、莱特宁一被动标记等效果)。',
   },
   急速翻滚: {
-    description: '只适合少部分老鼠。',
-    detailedDescription: '只适合少部分老鼠。用于补充位移和躲避技能，或是与部分技能产生联动。',
-    isMinor: true,
+    description: '只适合**少部分**老鼠。用于补充位移和躲避技能，或是与部分惯性类技能产生联动。',
   },
   冰冻保鲜: {
-    description: '只适合少部分老鼠，或用于对抗少数猫咪。',
-    detailedDescription:
-      '只适合少部分老鼠，或用于对抗少数猫咪。用于规避某些猫咪技能的效果，或是与部分技能产生联动。',
-    isMinor: true,
+    description:
+      '只适合**少部分**老鼠，或用于对抗**少部分**猫咪。用于规避某些猫咪技能的效果，或是与部分减控类技能产生联动。',
   },
   勇气投掷: {
-    description: '只适合极少部分老鼠。',
-    detailedDescription: '只适合极少部分老鼠。用于缩短部分技能CD。',
-    isMinor: true,
+    description: '只适合**极少部分**老鼠。用于缩短部分技能CD。',
   },
 };
 
--- src/components/displays/special-skills/special-skill-advice/SpecialSkillAdvice.tsx
+++ src/components/displays/special-skills/special-skill-advice/SpecialSkillAdvice.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import GameImage from '@/components/ui/GameImage';
-import specialSkillsAdvice from './SpecialSkillAdivceData';
+import specialSkillsAdvice, { SpecialSkillAdvice } from './SpecialSkillAdivceData';
 import type { FactionId } from '@/data/types';
 import { useState } from 'react';
 import PageTitle from '@/components/ui/PageTitle';
@@ -10,6 +10,14 @@ import BaseCard from '@/components/ui/BaseCard';
 import { getFactionButtonColors } from '@/lib/design-system';
 import { useDarkMode } from '@/context/DarkModeContext';
 import FilterRow from '@/components/ui/FilterRow';
+import { designTokens } from '@/lib/design-tokens';
+import TextWithHoverTooltips from '../../characters/shared/TextWithHoverTooltips';
+import { characters } from '@/data';
+import { CharacterWithFaction } from '@/lib/types';
+import Image from '@/components/Image';
+import Link from 'next/link';
+import { useMobile } from '@/hooks/useMediaQuery';
+import clsx from 'clsx';
 
 const allSkillsAdvice = [
   ...Object.values(specialSkillsAdvice.cat),
@@ -19,11 +27,50 @@ const allSkillsAdvice = [
 export default function SpecialSkillAdviceClient() {
   const [selectedFaction, setSelectedFaction] = useState<FactionId | null>(null);
   const [isDarkMode] = useDarkMode();
+  const isMobile = useMobile();
 
   // Filter skills by faction if selected
   const filteredSkills = selectedFaction
     ? allSkillsAdvice.filter((skill) => skill.factionId === selectedFaction)
     : allSkillsAdvice;
+
+  // Filter characters
+  function allCounteredBy(skill: SpecialSkillAdvice) {
+    const IsMinor: CharacterWithFaction[] = Object.values(characters).filter(
+      (character) =>
+        character.counteredBySpecialSkills?.some(
+          (s) => s.id === skill.name && s.isMinor === true
+        ) && character.factionId !== skill.factionId
+    );
+    const UnIsMinor: CharacterWithFaction[] = Object.values(characters).filter(
+      (character) =>
+        character.counteredBySpecialSkills?.some(
+          (s) => s.id === skill.name && s.isMinor === false
+        ) && character.factionId !== skill.factionId
+    );
+    return { 0: UnIsMinor, 1: IsMinor };
+  }
+  function allCounters(skill: SpecialSkillAdvice) {
+    const IsMinor: CharacterWithFaction[] = Object.values(characters).filter(
+      (character) =>
+        character.countersSpecialSkills?.some((s) => s.id === skill.name && s.isMinor === true) &&
+        character.factionId !== skill.factionId
+    );
+    const UnIsMinor: CharacterWithFaction[] = Object.values(characters).filter(
+      (character) =>
+        character.countersSpecialSkills?.some((s) => s.id === skill.name && s.isMinor === false) &&
+        character.factionId !== skill.factionId
+    );
+    return { 0: UnIsMinor, 1: IsMinor };
+  }
+  function allUsers(skill: SpecialSkillAdvice) {
+    const Return: CharacterWithFaction[] = Object.values(characters).filter(
+      (character) =>
+        character.specialSkills?.some((s) => s.name === skill.name) &&
+        character.factionId === skill.factionId
+    );
+    return Return;
+  }
 
   return (
     <div className='max-w-6xl mx-auto p-6 space-y-8 dark:text-slate-200'>
@@ -45,32 +92,268 @@ export default function SpecialSkillAdviceClient() {
           />
         </div>
       </header>
-      <div
-        className='auto-fit-grid grid-container grid gap-4 mt-8'
-        style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(150px, 1fr))' }}
-      >
+      <div className='auto-fit-grid grid-container grid gap-4 mt-8'>
         {filteredSkills.map((skill) => (
           <div
             key={skill.factionId + skill.name}
-            className='character-card transform transition-transform hover:-translate-y-1'
+            className='character-card transform transition-transform flex flex-col md:flex-row card dark:bg-slate-800 dark:border-slate-700'
           >
-            <BaseCard
-              variant='item'
-              href={`/special-skills/${encodeURIComponent(skill.factionId)}/${encodeURIComponent(
-                skill.name
-              )}`}
-              aria-label={`查看${skill.name}特技详情`}
+            <div className='md:w-1/5 '>
+              <BaseCard
+                variant='item'
+                href={`/special-skills/${encodeURIComponent(skill.factionId)}/${encodeURIComponent(
+                  skill.name
+                )}`}
+                aria-label={`查看${skill.name}特技详情`}
+                className='hover:-translate-y-1'
+              >
+                <GameImage
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  size='SPECIAL_SKILL_CARD'
+                  className='hover:scale-105'
+                />
+                <div className='px-3 pt-1 pb-3 text-center'>
+                  <div className='font-semibold dark:text-white'>{skill.name}</div>
+                </div>
+              </BaseCard>
+            </div>
+            <div
+              className='md:w-4/5'
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: designTokens.spacing.sm,
+                padding: designTokens.spacing.sm,
+              }}
             >
-              <GameImage
-                src={skill.imageUrl}
-                alt={skill.name}
-                size='SPECIAL_SKILL_CARD'
-                className='hover:scale-105'
-              />
-              <div className='px-3 pt-1 pb-3 text-center'>
-                <div className='font-semibold dark:text-white'>{skill.name}</div>
-              </div>
-            </BaseCard>
+              {/*description*/}
+              {skill.description !== undefined && (
+                <div>
+                  <p
+                    className='text-black dark:text-gray-200 text-lg'
+                    style={{
+                      paddingTop: designTokens.spacing.sm,
+                      paddingBottom: designTokens.spacing.sm,
+                    }}
+                  >
+                    <TextWithHoverTooltips text={skill.description as string} />
+                  </p>
+                </div>
+              )}
+
+              {/*counteredBy*/}
+              {(allCounteredBy(skill)[0].length != 0 || allCounteredBy(skill)[1].length != 0) && (
+                <div>
+                  <span className='font-semibold text-sm text-blue-700 dark:text-blue-300 flex items-center gap-1'>
+                    <span className='w-5 h-5 bg-blue-200 rounded-full flex items-center justify-center mr-1'>
+                      <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 16 16'
+                        fill='none'
+                        aria-label='smile'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <circle cx='5' cy='6' r='1.25' fill='#2563eb' />
+                        <circle cx='11' cy='6' r='1.25' fill='#2563eb' />
+                        <path
+                          d='M4 9.5 Q8 12.7 12 9.5'
+                          stroke='#2563eb'
+                          strokeWidth='2'
+                          fill='none'
+                          strokeLinecap='round'
+                        />
+                      </svg>
+                    </span>
+                    被{skill.name}克制的{skill.factionId == 'cat' ? '老鼠' : '猫咪'}：
+                  </span>
+                  <div className='rounded-xl border border-gray-200 dark:border-slate-700 bg-white dark:bg-slate-800 shadow-sm px-2 py-2'>
+                    <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                      {allCounteredBy(skill)[0].map((character) => (
+                        <li
+                          key={character.id ?? ''}
+                          className='flex items-center gap-4 p-3 rounded-lg transition-colors bg-blue-50 dark:bg-blue-900/30 hover:-translate-y-1'
+                        >
+                          <Link
+                            href={`/characters/${character.id}`}
+                            className='flex items-center gap-2 w-full'
+                            tabIndex={0}
+                          >
+                            <Image
+                              src={character.imageUrl!}
+                              alt={character.id!}
+                              className='w-10 h-10'
+                              width={40}
+                              height={40}
+                            />
+                            <span className='text-lg dark:text-white truncate'>{character.id}</span>
+                          </Link>
+                        </li>
+                      ))}
+                      {allCounteredBy(skill)[1].map((character) => (
+                        <li
+                          key={character.id ?? ''}
+                          className='flex items-center gap-4 p-3 rounded-lg transition-colors bg-blue-50 dark:bg-blue-900/30 opacity-60 hover:-translate-y-1'
+                        >
+                          <Link
+                            href={`/characters/${character.id}`}
+                            className='flex items-center gap-2 w-full opacity-80'
+                            tabIndex={0}
+                          >
+                            <Image
+                              src={character.imageUrl!}
+                              alt={character.id!}
+                              className='w-10 h-10'
+                              width={40}
+                              height={40}
+                            />
+                            <span className='text-lg dark:text-white truncate'>{character.id}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+
+              {/*counters*/}
+              {(allCounters(skill)[0].length != 0 || allCounters(skill)[1].length != 0) && (
+                <div>
+                  <span className='font-semibold text-sm text-red-700 dark:text-red-300 flex items-center gap-1'>
+                    <span className='w-5 h-5 bg-red-200 rounded-full flex items-center justify-center mr-1'>
+                      <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 16 16'
+                        fill='none'
+                        aria-label='sad'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <circle cx='5' cy='6' r='1.25' fill='#dc2626' />
+                        <circle cx='11' cy='6' r='1.25' fill='#dc2626' />
+                        <path
+                          d='M4 11 Q8 9.5 12 11'
+                          stroke='#dc2626'
+                          strokeWidth='2'
+                          fill='none'
+                          strokeLinecap='round'
+                        />
+                      </svg>
+                    </span>
+                    克制{skill.name}的{skill.factionId == 'cat' ? '老鼠' : '猫咪'}：
+                  </span>
+                  <div className='rounded-xl border border-gray-200 dark:border-slate-700 bg-white dark:bg-slate-800 shadow-sm px-2 py-2'>
+                    <ul className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+                      {allCounters(skill)[0].map((character) => (
+                        <li
+                          key={character.id ?? ''}
+                          className='flex items-center gap-4 p-3 rounded-lg transition-colors bg-red-50 dark:bg-red-900/30 hover:-translate-y-1'
+                        >
+                          <Link
+                            href={`/characters/${character.id}`}
+                            className='flex items-center gap-2 w-full'
+                            tabIndex={0}
+                          >
+                            <Image
+                              src={character.imageUrl!}
+                              alt={character.id!}
+                              className='w-10 h-10'
+                              width={40}
+                              height={40}
+                            />
+                            <span className='text-lg dark:text-white truncate'>{character.id}</span>
+                          </Link>
+                        </li>
+                      ))}
+                      {allCounters(skill)[1].map((character) => (
+                        <li
+                          key={character.id ?? ''}
+                          className='flex items-center gap-4 p-3 rounded-lg transition-colors bg-red-50 dark:bg-red-900/30 opacity-60 hover:-translate-y-1'
+                        >
+                          <Link
+                            href={`/characters/${character.id}`}
+                            className='flex items-center gap-2 w-full opacity-80'
+                            tabIndex={0}
+                          >
+                            <Image
+                              src={character.imageUrl!}
+                              alt={character.id!}
+                              className='w-10 h-10'
+                              width={40}
+                              height={40}
+                            />
+                            <span className='text-lg dark:text-white truncate'>{character.id}</span>
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+
+              {/*users*/}
+              {allUsers(skill).length != 0 && (
+                <div>
+                  <span className='font-semibold text-sm text-green-700 dark:text-green-300 flex items-center gap-1'>
+                    <span className='w-5 h-5 bg-green-200 rounded-full flex items-center justify-center mr-1'>
+                      <svg
+                        width='16'
+                        height='16'
+                        viewBox='0 0 16 16'
+                        fill='none'
+                        aria-label='heart'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <path
+                          d='M8 13 C8 13 3.5 10.5 3.5 7.5 C3.5 6 4.7 4.8 6.2 4.8 C7.1 4.8 7.8 5.2 8 5.9 C8.2 5.2 8.9 4.8 9.8 4.8 C11.3 4.8 12.5 6 12.5 7.5 C12.5 10.5 8 13 8 13 Z'
+                          fill='#bbf7d0'
+                          stroke='#16a34a'
+                          strokeWidth='1.8'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                        />
+                      </svg>
+                    </span>
+                    适合携带{skill.name}的{skill.factionId == 'cat' ? '猫咪' : '老鼠'}：
+                  </span>
+                  <div className='rounded-xl border border-gray-200 dark:border-slate-700 bg-white dark:bg-slate-800 shadow-sm px-2 py-2'>
+                    <ul
+                      className={clsx(
+                        'grid grid-cols-1 sm:grid-cols-2 gap-4',
+                        allUsers(skill).length <= 8 ? 'lg:grid-cols-4' : 'lg:grid-cols-10'
+                      )}
+                    >
+                      {allUsers(skill).map((character) => (
+                        <li
+                          key={character.id ?? ''}
+                          className='flex items-center gap-4 p-3 rounded-lg transition-colors bg-green-50 dark:bg-green-900/30 hover:-translate-y-1'
+                        >
+                          <Link
+                            href={`/characters/${character.id}`}
+                            className='flex items-center gap-2 w-full'
+                            tabIndex={0}
+                          >
+                            <Image
+                              src={character.imageUrl!}
+                              alt={character.id!}
+                              className='w-10 h-10'
+                              width={40}
+                              height={40}
+                            />
+                            {(isMobile || allUsers(skill).length <= 8) && (
+                              <span className='text-lg dark:text-white truncate'>
+                                {character.id}
+                              </span>
+                            )}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         ))}
       </div>
