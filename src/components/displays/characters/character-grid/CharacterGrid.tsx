af1a383705aa2005e0e2f8314a1b43e27702d384
ConductorJerry <1273590260@qq.com>
2025-09-17 18:34:13 +0800

    fix(CharacterGrid): change ui when useMobile

--- src/components/displays/characters/character-grid/CharacterGrid.tsx
+++ src/components/displays/characters/character-grid/CharacterGrid.tsx
@@ -19,12 +19,14 @@ import { getPositioningTagColors, getAvatarFilterColors } from '@/lib/design-tok
 import { useDarkMode } from '@/context/DarkModeContext';
 import PageTitle from '@/components/ui/PageTitle';
 import PageDescription from '@/components/ui/PageDescription';
+import { useMobile } from '@/hooks/useMediaQuery';
 
 export default function CharacterGrid({ faction }: FactionCharactersProps) {
   const { isDetailedView: isDetailed } = useAppContext();
   const { isEditMode } = useEditMode();
   const [isDarkMode] = useDarkMode();
   const originalCharacterIds = getOriginalCharacterIds();
+  const isMobile = useMobile();
 
   const originalCharacters = useMemo(() => {
     return originalCharacterIds
@@ -133,10 +135,18 @@ export default function CharacterGrid({ faction }: FactionCharactersProps) {
   }, [faction.id, selectedPositioningTags, isEditMode, originalCharacterIds, selectedAvatar]);
 
   return (
-    <div className='max-w-6xl mx-auto p-6 space-y-8 dark:text-slate-200'>
-      <header className='text-center space-y-4 mb-8 px-4'>
+    <div
+      className={
+        isMobile
+          ? 'max-w-3xl mx-auto p-2 space-y-2 dark:text-slate-200'
+          : 'max-w-6xl mx-auto p-6 space-y-8 dark:text-slate-200'
+      }
+    >
+      <header
+        className={isMobile ? 'text-center space-y-2 mb-4 px-2' : 'text-center space-y-4 mb-8 px-4'}
+      >
         <PageTitle>{faction.name}</PageTitle>
-        <PageDescription>{faction.description}</PageDescription>
+        {!isMobile && <PageDescription>{faction.description}</PageDescription>}
         {/* Filters wrapper */}
         <div className='space-y-0 mx-auto w-full max-w-2xl md:px-2'>
           <FilterRow
