c449948d9d9d3696cc5e639f11f08a136a78424e
3swordman <yyuxiaoran@qq.com>
2025-11-01 17:14:07 +0800

    fix(articles): fix build errors

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -4,18 +4,8 @@ import { Database } from '@/data/database.types';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
-if (process.env.NEXT_PUBLIC_DISABLE_ARTICLES) {
-  throw new Error('Missing env.SUPABASE_SERVICE_ROLE_KEY');
-}
-
-if (!supabaseUrl) {
-  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL');
-}
-
-if (!supabaseServiceRoleKey) {
-  throw new Error('Missing env.SUPABASE_SERVICE_ROLE_KEY');
-}
-
 // Note: this client is a singleton and can be used across the server-side of the app.
 // It has elevated privileges and should be used with caution.
-export const supabaseAdmin = createClient<Database>(supabaseUrl, supabaseServiceRoleKey);
+export const supabaseAdmin = process.env.NEXT_PUBLIC_DISABLE_ARTICLES
+  ? (void 0 as never)
+  : createClient<Database>(supabaseUrl!, supabaseServiceRoleKey!);
