de9559acbd956050b4c23f759027410b22c5cbb9
3swordman <yyuxiaoran@qq.com>
2025-11-22 22:34:49 +0800

    feat(characters): add recommended cat page (note that the page is hidden by default)

--- src/components/displays/characters/character-detail/CharacterRelationDisplay.tsx
+++ src/components/displays/characters/character-detail/CharacterRelationDisplay.tsx
@@ -4,7 +4,7 @@ import React, { useCallback, useState } from 'react';
 import Image from '@/components/Image';
 import { cards, FactionId, specialSkills } from '@/data';
 import { AssetManager } from '@/lib/assetManager';
-import { CharacterRelation, CharacterRelationItem } from '@/data/types';
+import { CharacterRelationItem } from '@/data/types';
 import { characters } from '@/data';
 import { useAppContext } from '@/context/AppContext';
 import { useEditMode, useLocalCharacter } from '@/context/EditModeContext';
@@ -717,99 +717,7 @@ function CharacterSelector({
   );
 }
 
-function getCharacterRelation(id: string): CharacterRelation {
-  const char = characters[id];
-  if (!char) {
-    return {
-      counters: [],
-      countersKnowledgeCards: [],
-      countersSpecialSkills: [],
-      counteredBy: [],
-      collaborators: [],
-      counteredByKnowledgeCards: [],
-      counterEachOther: [],
-      counteredBySpecialSkills: [],
-    };
-  }
-
-  const counteredBy = Object.values(characters)
-    .filter((c) => c.counters?.some((counter) => counter.id === id))
-    .map((c) => {
-      const restraintItem = c.counters?.find((counter) => counter.id === id);
-      const item =
-        restraintItem && typeof restraintItem.description === 'string'
-          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
-          : { id: c.id, isMinor: false };
-      return item;
-    });
-
-  const counterEachOther = Object.values(characters)
-    .filter((c) => c.counterEachOther?.some((counter) => counter.id === id))
-    .map((c) => {
-      const restraintItem = c.counterEachOther?.find((counter) => counter.id === id);
-      const item =
-        restraintItem && typeof restraintItem.description === 'string'
-          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
-          : { id: c.id, isMinor: false };
-      return item;
-    });
-
-  const counters = Object.values(characters)
-    .filter((c) => c.counteredBy?.some((countered) => countered.id === id))
-    .map((c) => {
-      const restraintItem = c.counteredBy?.find((countered) => countered.id === id);
-      const item =
-        restraintItem && typeof restraintItem.description === 'string'
-          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
-          : { id: c.id, isMinor: false };
-      return item;
-    });
-
-  const collaboratorsFromOthers = Object.values(characters)
-    .filter((c) => c.collaborators?.some((collab) => collab.id === id))
-    .map((c) => {
-      const collabItem = c.collaborators?.find((collab) => collab.id === id);
-      const item =
-        collabItem && typeof collabItem.description === 'string'
-          ? { id: c.id, description: collabItem.description, isMinor: !!collabItem.isMinor }
-          : { id: c.id, isMinor: false };
-      return item;
-    });
-
-  const ownCounters = (char.counters ?? []).map((item) => item);
-  const ownCounteredBy = (char.counteredBy ?? []).map((item) => item);
-  const ownCounterEachOther = (char.counterEachOther ?? []).map((item) => item);
-  const ownCollaborators = (char.collaborators ?? []).map((item) => item);
-
-  const mergedCounters = [
-    ...ownCounters,
-    ...counters.filter((c) => !ownCounters.some((oc) => oc.id === c.id)),
-  ];
-  const mergedCounteredBy = [
-    ...ownCounteredBy,
-    ...counteredBy.filter((c) => !ownCounteredBy.some((oc) => oc.id === c.id)),
-  ];
-  const mergedCounterEachOther = [
-    ...ownCounterEachOther,
-    ...counterEachOther.filter((c) => !ownCounterEachOther.some((oc) => oc.id === c.id)),
-  ];
-  const mergedCollaborators = [
-    ...ownCollaborators,
-    ...collaboratorsFromOthers.filter((c) => !ownCollaborators.some((oc) => oc.id === c.id)),
-  ];
-
-  return {
-    counters: mergedCounters,
-    countersKnowledgeCards: char.countersKnowledgeCards ?? [],
-    countersSpecialSkills: char.countersSpecialSkills ?? [],
-    counteredBy: mergedCounteredBy,
-    collaborators: mergedCollaborators,
-    counteredByKnowledgeCards: char.counteredByKnowledgeCards ?? [],
-    counteredBySpecialSkills: char.counteredBySpecialSkills ?? [],
-    counterEachOther: mergedCounterEachOther,
-  };
-}
-
+import { getCharacterRelation } from '@/lib/characterRelationUtils';
 const CharacterRelationDisplay: React.FC<Props> = ({ id, factionId }) => {
   const { isEditMode } = useEditMode();
   const { characterId } = useLocalCharacter();
---
+++ src/lib/characterRelationUtils.ts
@@ -0,0 +1,95 @@
+import { characters } from '@/data';
+import { CharacterRelation } from '@/data/types';
+
+export function getCharacterRelation(id: string): CharacterRelation {
+  const char = characters[id];
+  if (!char) {
+    return {
+      counters: [],
+      countersKnowledgeCards: [],
+      countersSpecialSkills: [],
+      counteredBy: [],
+      collaborators: [],
+      counteredByKnowledgeCards: [],
+      counterEachOther: [],
+      counteredBySpecialSkills: [],
+    };
+  }
+
+  const counteredBy = Object.values(characters)
+    .filter((c) => c.counters?.some((counter) => counter.id === id))
+    .map((c) => {
+      const restraintItem = c.counters?.find((counter) => counter.id === id);
+      const item =
+        restraintItem && typeof restraintItem.description === 'string'
+          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
+          : { id: c.id, isMinor: false };
+      return item;
+    });
+
+  const counterEachOther = Object.values(characters)
+    .filter((c) => c.counterEachOther?.some((counter) => counter.id === id))
+    .map((c) => {
+      const restraintItem = c.counterEachOther?.find((counter) => counter.id === id);
+      const item =
+        restraintItem && typeof restraintItem.description === 'string'
+          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
+          : { id: c.id, isMinor: false };
+      return item;
+    });
+
+  const counters = Object.values(characters)
+    .filter((c) => c.counteredBy?.some((countered) => countered.id === id))
+    .map((c) => {
+      const restraintItem = c.counteredBy?.find((countered) => countered.id === id);
+      const item =
+        restraintItem && typeof restraintItem.description === 'string'
+          ? { id: c.id, description: restraintItem.description, isMinor: !!restraintItem.isMinor }
+          : { id: c.id, isMinor: false };
+      return item;
+    });
+
+  const collaboratorsFromOthers = Object.values(characters)
+    .filter((c) => c.collaborators?.some((collab) => collab.id === id))
+    .map((c) => {
+      const collabItem = c.collaborators?.find((collab) => collab.id === id);
+      const item =
+        collabItem && typeof collabItem.description === 'string'
+          ? { id: c.id, description: collabItem.description, isMinor: !!collabItem.isMinor }
+          : { id: c.id, isMinor: false };
+      return item;
+    });
+
+  const ownCounters = (char.counters ?? []).map((item) => item);
+  const ownCounteredBy = (char.counteredBy ?? []).map((item) => item);
+  const ownCounterEachOther = (char.counterEachOther ?? []).map((item) => item);
+  const ownCollaborators = (char.collaborators ?? []).map((item) => item);
+
+  const mergedCounters = [
+    ...ownCounters,
+    ...counters.filter((c) => !ownCounters.some((oc) => oc.id === c.id)),
+  ];
+  const mergedCounteredBy = [
+    ...ownCounteredBy,
+    ...counteredBy.filter((c) => !ownCounteredBy.some((oc) => oc.id === c.id)),
+  ];
+  const mergedCounterEachOther = [
+    ...ownCounterEachOther,
+    ...counterEachOther.filter((c) => !ownCounterEachOther.some((oc) => oc.id === c.id)),
+  ];
+  const mergedCollaborators = [
+    ...ownCollaborators,
+    ...collaboratorsFromOthers.filter((c) => !ownCollaborators.some((oc) => oc.id === c.id)),
+  ];
+
+  return {
+    counters: mergedCounters,
+    countersKnowledgeCards: char.countersKnowledgeCards || [],
+    countersSpecialSkills: char.countersSpecialSkills || [],
+    counteredBy: mergedCounteredBy,
+    counteredByKnowledgeCards: char.counteredByKnowledgeCards || [],
+    counteredBySpecialSkills: char.counteredBySpecialSkills || [],
+    counterEachOther: mergedCounterEachOther,
+    collaborators: mergedCollaborators,
+  };
+}
