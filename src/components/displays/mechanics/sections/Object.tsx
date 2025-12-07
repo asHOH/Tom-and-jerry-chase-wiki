d3027e868356138b85c05649431d4b2fb2910b34
asHOH <75458945+asHOH@users.noreply.github.com>
2025-12-07 20:15:51 +0800

    fix(ObjectCollision): wrap MDXContent in StyledMDX to apply styles correctly

--- src/components/displays/mechanics/sections/Object.tsx
+++ src/components/displays/mechanics/sections/Object.tsx
@@ -1,7 +1,13 @@
 'use client';
 
+import StyledMDX from '@/components/ui/StyledMDX';
+
 import MDXContent from '../articles/Object.mdx';
 
 export default function ObjectCollision() {
-  return <MDXContent />;
+  return (
+    <StyledMDX className='article-content'>
+      <MDXContent />
+    </StyledMDX>
+  );
 }
