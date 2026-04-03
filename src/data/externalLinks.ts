5b05e37e65152a507ef6dc79acd1ce6e973eeb99
3swordman <yyuxiaoran@qq.com>
2026-04-03 23:54:57 +0800

    feat(ui): add correct svg icons for official websites

--- src/data/externalLinks.ts
+++ src/data/externalLinks.ts
@@ -3,6 +3,7 @@ export type ExternalLinkItem = {
   description: string;
   ariaLabel: string;
   href?: string;
+  iconType?: 'fandom' | 'netease' | 'weibo' | 'bilibili';
 };
 
 export type ExternalLinkGroup = {
@@ -16,10 +17,11 @@ export const EXTERNAL_LINK_GROUPS: ExternalLinkGroup[] = [
     title: '友情链接',
     items: [
       {
-        title: 'Fandom Wiki',
+        title: 'Fandom',
         description: '英文社区资料站',
         ariaLabel: '打开 Tom and Jerry: Chase Fandom Wiki',
         href: 'https://tom-and-jerry-chase.fandom.com/wiki/Tom_and_Jerry:_Chase_Wiki',
+        iconType: 'fandom',
       },
     ],
   },
@@ -32,18 +34,21 @@ export const EXTERNAL_LINK_GROUPS: ExternalLinkGroup[] = [
         description: '官方号动态与活动',
         ariaLabel: '打开猫和老鼠手游网易大神官方号',
         href: 'https://ds.163.com/user/9c1c00448e28455781e8fde62830160d/',
+        iconType: 'netease',
       },
       {
         title: '微博',
         description: '官方资讯与公告',
         ariaLabel: '打开猫和老鼠官方手游微博',
         href: 'https://weibo.com/tnj163',
+        iconType: 'weibo',
       },
       {
         title: '哔哩哔哩',
         description: '官方视频与栏目',
         ariaLabel: '打开猫和老鼠官方手游哔哩哔哩主页',
         href: 'https://space.bilibili.com/298683153',
+        iconType: 'bilibili',
       },
     ],
   },
